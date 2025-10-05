d79164cdd89ae848845e2476c15f3fefa7a53a18
Josue Rivera <jsuerivera09@gmail.com>
2025-10-05 15:30:22 -0600

    Primer commit del proyecto,agrego carpeta SQLy .gitignore

---
+++ Restaurante_app/Restaurante_app/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Restaurante_app
+{
+    partial class restaurante_app
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.btnSalir = new FontAwesome.Sharp.IconButton();
+            this.btnReporte = new FontAwesome.Sharp.IconButton();
+            this.btnRegistro = new FontAwesome.Sharp.IconButton();
+            this.btnGestion = new FontAwesome.Sharp.IconButton();
+            this.btnMenu = new FontAwesome.Sharp.IconButton();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.ImageLogo = new System.Windows.Forms.PictureBox();
+            this.panelTitulo = new System.Windows.Forms.Panel();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.panelShadow = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelPrincipal.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageLogo)).BeginInit();
+            this.panelTitulo.SuspendLayout();
+            this.panelContenedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelPrincipal.Controls.Add(this.btnSalir);
+            this.panelPrincipal.Controls.Add(this.btnReporte);
+            this.panelPrincipal.Controls.Add(this.btnRegistro);
+            this.panelPrincipal.Controls.Add(this.btnGestion);
+            this.panelPrincipal.Controls.Add(this.btnMenu);
+            this.panelPrincipal.Controls.Add(this.panelLogo);
+            this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(170, 476);
+            this.panelPrincipal.TabIndex = 0;
+            this.panelPrincipal.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPrincipal_Paint);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.AllowDrop = true;
+            this.btnSalir.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.ForeColor = System.Drawing.Color.White;
+            this.btnSalir.IconChar = FontAwesome.Sharp.IconChar.RightFromBracket;
+            this.btnSalir.IconColor = System.Drawing.Color.White;
+            this.btnSalir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSalir.IconSize = 32;
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(0, 416);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnSalir.Rotation = 180D;
+            this.btnSalir.Size = new System.Drawing.Size(170, 60);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReporte.FlatAppearance.BorderSize = 0;
+            this.btnReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporte.ForeColor = System.Drawing.Color.White;
+            this.btnReporte.IconChar = FontAwesome.Sharp.IconChar.ChartSimple;
+            this.btnReporte.IconColor = System.Drawing.Color.White;
+            this.btnReporte.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnReporte.IconSize = 32;
+            this.btnReporte.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReporte.Location = new System.Drawing.Point(0, 307);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnReporte.Size = new System.Drawing.Size(170, 60);
+            this.btnReporte.TabIndex = 4;
+            this.btnReporte.Text = "Reportes Venta";
+            this.btnReporte.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReporte.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegistro.FlatAppearance.BorderSize = 0;
+            this.btnRegistro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistro.ForeColor = System.Drawing.Color.White;
+            this.btnRegistro.IconChar = FontAwesome.Sharp.IconChar.SackDollar;
+            this.btnRegistro.IconColor = System.Drawing.Color.White;
+            this.btnRegistro.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnRegistro.IconSize = 32;
+            this.btnRegistro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistro.Location = new System.Drawing.Point(0, 247);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnRegistro.Size = new System.Drawing.Size(170, 60);
+            this.btnRegistro.TabIndex = 3;
+            this.btnRegistro.Text = "Registro Ventas";
+            this.btnRegistro.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistro.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // btnGestion
+            // 
+            this.btnGestion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnGestion.FlatAppearance.BorderSize = 0;
+            this.btnGestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestion.ForeColor = System.Drawing.Color.White;
+            this.btnGestion.IconChar = FontAwesome.Sharp.IconChar.FileInvoiceDollar;
+            this.btnGestion.IconColor = System.Drawing.Color.White;
+            this.btnGestion.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnGestion.IconSize = 32;
+            this.btnGestion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGestion.Location = new System.Drawing.Point(0, 187);
+            this.btnGestion.Name = "btnGestion";
+            this.btnGestion.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnGestion.Size = new System.Drawing.Size(170, 60);
+            this.btnGestion.TabIndex = 2;
+            this.btnGestion.Text = "Gestión";
+            this.btnGestion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGestion.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGestion.UseVisualStyleBackColor = true;
+            this.btnGestion.Click += new System.EventHandler(this.btnGestion_Click);
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMenu.FlatAppearance.BorderSize = 0;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMenu.ForeColor = System.Drawing.Color.White;
+            this.btnMenu.IconChar = FontAwesome.Sharp.IconChar.BookOpen;
+            this.btnMenu.IconColor = System.Drawing.Color.White;
+            this.btnMenu.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMenu.IconSize = 32;
+            this.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMenu.Location = new System.Drawing.Point(0, 127);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnMenu.Size = new System.Drawing.Size(170, 60);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.Text = "Menú ";
+            this.btnMenu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMenu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.ImageLogo);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(170, 127);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // ImageLogo
+            // 
+            this.ImageLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImageLogo.Image = global::Restaurante_app.Properties.Resources.restaurante_rotulacion_composicion_caligrafica_diseno_vintage;
+            this.ImageLogo.Location = new System.Drawing.Point(0, 0);
+            this.ImageLogo.Name = "ImageLogo";
+            this.ImageLogo.Size = new System.Drawing.Size(170, 127);
+            this.ImageLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageLogo.TabIndex = 0;
+            this.ImageLogo.TabStop = false;
+            this.ImageLogo.Click += new System.EventHandler(this.ImageLogo_Click);
+            // 
+            // panelTitulo
+            // 
+            this.panelTitulo.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelTitulo.Controls.Add(this.labelUser);
+            this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulo.Location = new System.Drawing.Point(170, 0);
+            this.panelTitulo.Name = "panelTitulo";
+            this.panelTitulo.Size = new System.Drawing.Size(663, 70);
+            this.panelTitulo.TabIndex = 1;
+            this.panelTitulo.Paint += new System.Windows.Forms.PaintEventHandler(this.panelTitulo_Paint);
+            this.panelTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitulo_MouseDown);
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.ForeColor = System.Drawing.Color.White;
+            this.labelUser.Location = new System.Drawing.Point(45, 23);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(15, 24);
+            this.labelUser.TabIndex = 0;
+            this.labelUser.Text = " ";
+            // 
+            // panelShadow
+            // 
+            this.panelShadow.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panelShadow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelShadow.Location = new System.Drawing.Point(170, 70);
+            this.panelShadow.Name = "panelShadow";
+            this.panelShadow.Size = new System.Drawing.Size(663, 5);
+            this.panelShadow.TabIndex = 2;
+            this.panelShadow.Paint += new System.Windows.Forms.PaintEventHandler(this.panelShadow_Paint);
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panelContenedor.Controls.Add(this.pictureBox1);
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(170, 75);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(663, 401);
+            this.panelContenedor.TabIndex = 3;
+            this.panelContenedor.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContenedor_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Restaurante_app.Properties.Resources.restaurante_rotulacion_composicion_caligrafica_diseno_vintage;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(663, 401);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // restaurante_app
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(833, 476);
+            this.ControlBox = false;
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panelShadow);
+            this.Controls.Add(this.panelTitulo);
+            this.Controls.Add(this.panelPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "restaurante_app";
+            this.Text = "Restaurante";
+            this.panelPrincipal.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageLogo)).EndInit();
+            this.panelTitulo.ResumeLayout(false);
+            this.panelTitulo.PerformLayout();
+            this.panelContenedor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox ImageLogo;
+        private FontAwesome.Sharp.IconButton btnMenu;
+        private FontAwesome.Sharp.IconButton btnSalir;
+        private FontAwesome.Sharp.IconButton btnReporte;
+        private FontAwesome.Sharp.IconButton btnRegistro;
+        private FontAwesome.Sharp.IconButton btnGestion;
+        private System.Windows.Forms.Panel panelTitulo;
+        private System.Windows.Forms.Panel panelShadow;
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.PictureBox pictureBox1;
+       
+    }
+}
+
---
+++ Restaurante_app/Restaurante_app/menuForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurante_app
+{
+    public partial class menuForm : Form
+    {
+        public menuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void menuForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void panelContenedorMenu_Paint(object sender, PaintEventArgs e)
+        {
+           panelContenedorMenu.BackColor = Color.FromArgb(31, 29, 43);
+        }
+    }
+}
